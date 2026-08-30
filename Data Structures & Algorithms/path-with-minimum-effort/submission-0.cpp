7f7d73aa536dffaac1fe6eeda6e2b2c43e4dc604
Bhupesh Chavan <138667885+Bhupesh76@users.noreply.github.com>
2026-08-30 13:36:54 +0530

    Add: path-with-minimum-effort - submission-0

---
+++ Data Structures & Algorithms/path-with-minimum-effort/submission-0.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) 
+    {
+        int n = heights.size();
+        int m = heights[0].size();
+
+        priority_queue<pair<int,pair<int,int>>
+        ,vector<pair<int,pair<int,int>>>
+        ,greater<pair<int,pair<int,int>>>> pq;
+
+        vector<vector<int>> dist(n,vector<int>(m,1e9));
+        dist[0][0] = 0;
+
+        pq.push({0,{0,0}});
+
+        int delrow [] = {-1,0,1,0};
+        int delcol [] = {0,1,0,-1};
+
+        while(!pq.empty())
+        {
+            auto it = pq.top();
+            pq.pop();
+            int diff = it.first;
+            int r = it.second.first;
+            int c = it.second.second;
+
+            if(r == n-1 && c == m-1) return diff;
+
+            for(int i=0; i<4; i++)
+            {
+                int nrow = r+delrow[i];
+                int ncol = c+delcol[i];
+
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m)
+                {
+                    int mineffort = max(abs(heights[r][c] - heights[nrow][ncol]),diff);
+                    if(mineffort < dist[nrow][ncol])
+                    {
+                        dist[nrow][ncol] = mineffort;
+                        pq.push({mineffort,{nrow,ncol}});
+                    }    
+                }
+            }
+        }
+        return 0;
+    }
+};

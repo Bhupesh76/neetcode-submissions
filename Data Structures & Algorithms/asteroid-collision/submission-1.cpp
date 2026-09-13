faffd6d69871a0eeb3fc8b423c38792d5cb49d1d
Bhupesh Chavan <138667885+Bhupesh76@users.noreply.github.com>
2026-09-13 11:18:54 +0530

    Add: asteroid-collision - submission-1

---
+++ Data Structures & Algorithms/asteroid-collision/submission-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) 
+    {
+        stack<int> st;
+        vector<int> ans;
+        for(int x:asteroids)
+        {
+            if(x>0)
+            {
+                st.push(x);
+            }
+            else
+            {
+                while(!st.empty() && st.top() > 0 && st.top() < abs(x))
+                {
+                    st.pop();
+                }
+
+                if(!st.empty() && st.top() == abs(x))
+                {
+                    st.pop();
+                }
+                else if(st.empty() || st.top() < 0)
+                {
+                    st.push(x);
+                }
+            }
+        }
+        while(!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};

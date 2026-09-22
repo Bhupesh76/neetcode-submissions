f2e1f8cb6546a2587824d64d2093b1fb0962b129
Bhupesh Chavan <138667885+Bhupesh76@users.noreply.github.com>
2026-09-22 11:14:44 +0530

    Add: sort-an-array - submission-1

---
+++ Data Structures & Algorithms/sort-an-array/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& arr) 
+    {
+        
+        for(int i=arr.size()-1; i>=1; i--)
+        {
+            for(int j=0; j<i; j++)
+            {
+                if(arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+};
